292e03916a9c8c528020284a72a462eb671d8a10
studsX <42314844+smbhacks@users.noreply.github.com>
2026-05-23 21:00:28 +0200

    solid and fragile platform

--- tigri-game/GameplayScene.cpp
+++ tigri-game/GameplayScene.cpp
@@ -1,6 +1,8 @@
 #include "GameplayScene.h"
 #include "memtrace.h"
 #include "GameUtils.h"
+#include "SolidPlatform.h"
+#include "FragilePlatform.h"
 
 GameplayScene::GameplayScene()
 	: Scene()
@@ -11,8 +13,8 @@ GameplayScene::GameplayScene()
 	, m_bgMusic("src/be_higher.mp3")
 	, m_player(200.0f, -70.0f, Box(-75, -30, 150, 73), m_platforms)
 {
-	m_platforms.push_back(std::make_unique<Platform>(-0.6f, -0.2f, 200.0f, GameUtils::getRandomNum(500.0f, 650.0f)));
-	m_platforms.push_back(std::make_unique<Platform>(-1.3f, -0.1f, 700.0f, GameUtils::getRandomNum(400.0f, 650.0f)));
+	m_platforms.push_back(std::make_unique<SolidPlatform>(-0.6f, -0.2f, 200.0f, GameUtils::getRandomNum(500.0f, 650.0f)));
+	m_platforms.push_back(std::make_unique<SolidPlatform>(-1.3f, -0.1f, 700.0f, GameUtils::getRandomNum(400.0f, 650.0f)));
 	SystemUtils::playMusic(m_bgMusic, -1);
 }
 
@@ -25,7 +27,7 @@ float GameplayScene::m_getRandomPlatformSpeed()
 	{
 		m_platformBaseSpeed -= 0.4f;
 		GameUtils::clamp(m_platformBaseSpeed, minBaseSpeed, std::numeric_limits<float>::max());
-		size_t newRestartVal = timer.getRestartVal() + 120;
+		size_t newRestartVal = timer.getRestartVal() + 90;
 		timer.setRestartVal(newRestartVal);
 		timer.restart();
 	}
@@ -54,7 +56,18 @@ void GameplayScene::tick()
 	{
 		float xSpeed = m_getRandomPlatformSpeed();
 		float ySpeed = GameUtils::getRandomNum(-0.5f, 0.5f);
-		m_platforms.push_back(std::make_unique<Platform>(xSpeed, ySpeed, m_camera.getX()+1280.0f, GameUtils::getRandomNum(400.0f, 650.0f)));
+		float xPos = m_camera.getX() + 1280.0f;
+		float yPos = GameUtils::getRandomNum(400.0f, 650.0f);
+		std::unique_ptr<Platform> platformPtr;
+		float randomVal = GameUtils::getRandomNum(0.0f, 1.0f);
+		if (randomVal < 0.33)
+		{
+			m_platforms.push_back(std::make_unique<FragilePlatform>(xSpeed, ySpeed, xPos, yPos));
+		}
+		else
+		{
+			m_platforms.push_back(std::make_unique<SolidPlatform>(xSpeed, ySpeed, xPos, yPos));
+		}
 		size_t maxTimeToWait = size_t(700.0f / abs(xSpeed));
 		m_platformSpawnTimer.setRestartVal(GameUtils::getRandomNum<size_t>(10, maxTimeToWait));
 		m_platformSpawnTimer.restart();
@@ -64,10 +77,11 @@ void GameplayScene::tick()
 
 void GameplayScene::draw()
 {
-	m_player.draw(m_camera);
-	m_player.drawCollBox(m_camera);
 	for (auto& platform : m_platforms)
 	{
-       platform->drawCollBox(m_camera);
+		platform->draw(m_camera);
+		platform->drawCollBox(m_camera);
 	}
+	m_player.draw(m_camera);
+	m_player.drawCollBox(m_camera);
 }
